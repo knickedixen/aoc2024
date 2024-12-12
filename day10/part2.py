948383cf27954659c13a9d682a37e39a7147f955
Joel Eriksson <knickedixen@gmail.com>
2024-12-13 00:18:26 +0100

    day10

---
+++ day10/part2.py
@@ -0,0 +1,33 @@
+
+with open("input.txt") as file:
+    map = [list(line.rstrip()) for line in file]
+
+result = 0
+max = len(map)
+
+def check(x,y,p):
+    if x >= max or y >= max or y < 0 or x < 0:
+        return []
+    h = int(map[x][y])
+    if h-p != 1:
+        return []
+    if h == 9:
+        return [f"{x},{y}"]
+
+    return explore(x,y,h)
+
+def explore(x,y,p):
+    res = []
+    res.extend(check(x+1,y,p))
+    res.extend(check(x-1,y,p))
+    res.extend(check(x,y+1,p))
+    res.extend(check(x,y-1,p))
+
+    return res 
+
+for x in range(max):
+    for y in range(len(map[x])):
+        if int(map[x][y]) == 0:
+            result += len(explore(x,y,0))
+
+print(result)
